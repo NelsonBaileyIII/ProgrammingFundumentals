0418239af7f9949ea7cc5402c5cb8e83f3411b9a
NelsonBaileyIII <nelsonbaileyiii@gmail.com>
2020-11-01 12:56:25 -0500

    Digital Crafts Homework for Javascript

---
+++ actor-exercises.js
@@ -0,0 +1,82 @@
+
+// let objectArray = ["2","fish","red", "green", "1"] 
+// function findFirstElement(objectArray){
+//     console.log(objectArray[0]);
+//     console.log(objectArray[3]);
+//     console.log(objectArray[Math.floor(Math.random()*objectArray.length)]);
+//     objectArray.push("Silver")
+//     console.log(objectArray[5])
+
+// }
+// findFirstElement(objectArray);
+// loopArray = [1,4,5,8,2]
+// for (let i =0; i <loopArray.length; i++){
+//     if(loopArray[i] > 5)
+//         console.log(loopArray[i]);
+// }
+
+// for(let i = loopArray.length; i >= 0;i--){
+//     console.log(loopArray[i])
+
+// // }
+// // let n = 1;
+//while(n < 10){
+    //console.log(n)
+   // n++;
+//    // if(n === 10)}
+// let = foundnumber = 0;
+// loopArray1 = [1,4,5,6,2,8]
+// for (let n=0; n > loopArray1.length; n++){
+//     console.log(loopArray1[n]);
+//     if(loopArray1[n] === 4)
+//         foundnumber = n;
+// }
+
+// let n = 1 
+// while (n <  20) {
+//     n++;
+// }
+// console.log(n)
+//  for (var n=0; n<= 20; n++) {
+//      if (n === 0) {
+//          console.log(n + " is even");
+//      }
+//     else if (n % 2 === 0) {
+//           console.log(n + " is even");
+//     }
+//     else {
+//         console.log(n + " is odd");
+//     }
+// }
+// let mutiple = 9;
+// for ( let i = 1; i <= 10; i++) {
+//     mutiple = mutiple * i;
+//     console.log(mutiple);
+
+// }
+// let counter = 100;
+// while (counter >= 0);{
+//     console.log(counter);
+//     counter--;
+// }
+
+// for ( let hour = 0; hour < 24; hour++){
+//     console.log('The current hour is ')
+// }
+
+
+// columns = ["A","B","C"]
+// rows = [1,2,3]
+// for (let column =0; column < columns.length; column++){
+//     for(let row = 0; row < rows.length; row++){
+//         console.log(`This is column: ${columns[column]} and this is row: ${rows[row]}`)
+
+//     }
+problemArray = [['Lion', 'Deer', 'Sheep'],['Cub', 'Fawn', 'Lamb']]
+for(let i=0; i < problemArray; i++ ){
+    console.log(problemArray[i])
+}
+
+
+
+
---
+++ recipe-exercises/scripts.js
@@ -0,0 +1,13 @@
+function marinaraSauce(cookName, two, onefourth){
+
+    // also use var two = "2" and var onefourth = "1/4" to get the same results. 
+    console.log("Hey, " + cookName + " this is my marinara sauce recipe!")
+    console.log("Heat a medium-large saucepan over medium heat") 
+    console.log("Add " + two + " tspns of oil and 5 carlic cloves, cook until golden, about 2 minute")
+    console.log("Add " + onefourth + " cup water, 2 cans of crushed tomates, and salt and season with black pepper to taste.")
+    console.log("Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes")
+    console.log("Stir in " + onefourth + " cup roughly chopped fresh basil, salt and pepper as needed.")
+}
+    marinaraSauce("Adam","23","12");
+    marinaraSauce("Nelson","100","2")
+    
